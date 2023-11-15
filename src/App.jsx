7079b6a75d3dd0097faea7bf56dd7aa1b42f7975
Jesus Hernandez <jesushernandez@Jesuss-MacBook-Pro-2.local>
2023-11-15 15:56:51 -0500

    const contactList

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-
+import ContactList from './components/ContactList'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+
 
   return (
     <>
-  
+      <ContactList />
     </>
   )
 }
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ContactList = () => {
+
+    return (
+    <table>
+        <thead>
+          <tr>
+            <th colSpan="3">Conttact List</th>
+         </tr>
+        </thead>
+    
+     <tbody>
+            <tr>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Phone</td>
+            </tr>
+            {
+
+            }
+     </tbody>
+    </table>
+    
+
+
+
+
+    );
+
+}
+
+export default ContactList;
