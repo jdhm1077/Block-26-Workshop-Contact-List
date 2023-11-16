ae32cff9c6e1b8f1d881405b651ccfe4e56826a8
Jesus Hernandez <jesushernandez@Jesuss-MacBook-Pro-2.local>
2023-11-16 10:12:31 -0500

    useEffect and fetching

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useState } from 'react'
 import ContactRow from "./ContactRow"
+import {useEffect} from 'react'
+
+
+
+
 
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -15,7 +20,21 @@ const ContactList = () => {
     
 
       const [contacts, setContacts] = useState(dummyContacts);
-       console.log("Contacts: ", contacts);
+      // console.log("Contacts: ", contacts);
+      useEffect(() => {
+        async function fetchContacts() {
+          try {
+            const response = await fetch(
+              "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+            );
+            const result = await response.json();
+            setContacts(result);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+        fetchContacts();
+      }, []);
 
     return (
     <table>
